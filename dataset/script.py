41553204041d288759223f083dfb2caff4e442c3
pillai-amal <mail.vortextv@gmail.com>
2023-11-21 15:20:38 +0200

    sim done

--- dataset/script.py
+++ dataset/script.py
@@ -1,46 +1,37 @@
 import bpy
 import os
+import re
 
-def import_and_setup_model(filepath, decimate_ratio):
-    bpy.ops.import_scene.obj(
-        filepath=filepath,
-        axis_forward='-Z',
-        axis_up='Y',
-        filter_glob="*.obj;*.mtl"
-    )
-
-    imported_object = bpy.context.selected_objects[0]
-    bpy.context.view_layer.objects.active = imported_object
-    bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS', center='BOUNDS')
-    bpy.ops.transform.resize(value=(1, 1, 1))
+def import_and_setup_model(filepath):
+    directory = re.sub(r"\\[^\\]*$", "", filepath)
+    bpy.ops.wm.obj_import(filepath=filepath,directory=directory)
     
-    bpy.ops.object.modifier_add(type='DECIMATE')
-    bpy.context.object.modifiers["Decimate"].ratio = decimate_ratio
-    bpy.ops.object.modifier_apply({"object": imported_object}, modifier="Decimate")
+    obj_name = bpy.context.object.name
+    max_dim = max(bpy.data.objects.get(obj_name).dimensions)
+    
+    bpy.context.object.scale[0] = 1/max_dim
+    bpy.context.object.scale[1] = 1/max_dim
+    bpy.context.object.scale[2] = 1/max_dim
+
+    bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS', center='BOUNDS')
 
     bpy.ops.object.location_clear(clear_delta=False)
     bpy.ops.object.modifier_add(type='COLLISION')
     bpy.context.view_layer.objects.active = None
 
-    size_x, size_y, size_z = imported_object.dimensions
-
-    return [size_x, size_y, size_z]
-
-def setup_cloth_simulation(subdivisions,x,y):
+def setup_cloth_simulation():
     bpy.ops.mesh.primitive_plane_add(
         enter_editmode=False,
         align='WORLD',
-        location=(0, 0, 60),
-        scale=(x, y, 1)
+        location=(0, 0, 20),
+        scale=(2, 2, 1)
     )
-    
-    bpy.ops.transform.resize(value=(x, y, 1))
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.subdivide(number_cuts=100)
     bpy.ops.object.mode_set(mode='OBJECT')  # Switch back to Object mode
-
-    # cloth_object = bpy.context.active_object
     bpy.ops.object.modifier_add(type='CLOTH')
     
-    #Additional cloth settings which can be turned on if necessary 
+    # Additional cloth settings which can be turned on if necessary 
 
     # bpy.context.object.modifiers["Cloth"].settings.quality = 5
     # bpy.context.object.modifiers["Cloth"].settings.use_pressure = True
@@ -52,23 +43,11 @@ def setup_cloth_simulation(subdivisions,x,y):
     bpy.context.scene.frame_end = 250  # Adjust frame_end as needed
     bpy.ops.ptcache.bake_all(bake=True)
 
-def setup_lighting():
-    bpy.ops.object.light_add(type='POINT', radius=1, align='WORLD', location=(0, 0, 200))
-    light = bpy.context.object
-    light.data.energy = 1000
-
 def export_mesh(obj, filepath):
     bpy.ops.object.select_all(action='DESELECT')
     obj.select_set(True)
     bpy.context.view_layer.objects.active = obj
     bpy.ops.export_scene.obj(filepath=filepath, use_selection=True)
-
-def setup_camera():
-    bpy.ops.object.camera_add(enter_editmode=False, align='WORLD', location=(0, -10, 2), rotation=(1.22, 0, 0))
-    bpy.context.scene.camera = bpy.context.object
-
-    # Set wider focal length
-    bpy.data.cameras[bpy.context.camera.data.name].lens = 35  # Adjust the value as needed
 
 def main():
     bpy.ops.object.select_all(action='DESELECT')
@@ -79,30 +58,21 @@ def main():
     models = ["monkey.obj"]
 
     for model in models:
-        #import_and_setup_model(model, decimate_ratio=0.5)
-        x, y, _ = import_and_setup_model(model, decimate_ratio=0.5)
-        setup_cloth_simulation(22,x,y)
+        import_and_setup_model(model)
+        setup_cloth_simulation()
 
-        cloth_object = bpy.context.active_object
+        cloth_object = bpy.context.collection.objects[0]
         cloth_export_path = os.path.join(os.getcwd(), f"{model}_cloth.obj")
         export_mesh(cloth_object, cloth_export_path)
 
-        original_object = bpy.context.selected_objects[0]
+        original_object = bpy.context.collection.objects[1]
         original_export_path = os.path.join(os.getcwd(), f"{model}_original.obj")
         export_mesh(original_object, original_export_path)
-
-    setup_lighting()
-    setup_camera()
-
-    bpy.context.scene.render.image_settings.file_format = 'PNG'
-    bpy.context.scene.render.filepath = output_path
 
     bpy.ops.render.render(write_still=True)
 
     print("Import, Transformation, and Cloth Simulation completed.")
-    print(f"Image saved at: {output_path}")
     print(f"Cloth saved at: {cloth_export_path}")
-    print(f"object saved at: {original_export_path}")
+    print(f"Object saved at: {original_export_path}")
 
-output_path = os.path.join(os.getcwd(), "rendered_image.png")
 main()
