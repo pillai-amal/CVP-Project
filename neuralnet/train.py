6c8911c1abd26022b679ccf7405e1148a746df18
Jibak Sarkar <jibaksarkar@yahoo.in>
2024-02-07 12:35:28 +0100

    working code

--- neuralnet/train.py
+++ neuralnet/train.py
@@ -1,32 +1,118 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+# from torch.utils.data import DataLoader
 from tqdm import tqdm
-from model import UNetPixelwiseRegression
-from dataset import CustomDataset
+# from model import UNetPixelwiseRegression
+from unet import UNET
+from dataset import CVPDataset
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_prediction_as_imgs
+)
 
-def train_model(model, dataloader, criterion, optimizer, num_epochs):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
-    for epoch in range(num_epochs):
-        model.train()  # Set the model to training mode
-        total_loss = 0.0
+# def train_model(model, dataloader, criterion, optimizer, num_epochs):
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     model.to(device)
 
-        with tqdm(total=len(dataloader), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
-            for inputs, targets in dataloader:
-                inputs, targets = inputs.to(device), targets.to(device)
+#     for epoch in range(num_epochs):
+#         model.train()  # Set the model to training mode
+#         total_loss = 0.0
 
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = criterion(outputs, targets)
-                loss.backward()
-                optimizer.step()
+#         with tqdm(total=len(dataloader), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
+#             for inputs, targets in dataloader:
+#                 inputs, targets = inputs.to(device), targets.to(device)
 
-                total_loss += loss.item()
-                pbar.set_postfix(loss=f'{loss.item():.4f}')
-                pbar.update()
+#                 optimizer.zero_grad()
+#                 outputs = model(inputs)
+#                 loss = criterion(outputs, targets)
+#                 loss.backward()
+#                 optimizer.step()
 
-        average_loss = total_loss / len(dataloader)
-        print(f'Epoch {epoch+1}/{num_epochs}, Average Loss: {average_loss:.4f}')
+#                 total_loss += loss.item()
+#                 pbar.set_postfix(loss=f'{loss.item():.4f}')
+#                 pbar.update()
+
+#         average_loss = total_loss / len(dataloader)
+#         print(f'Epoch {epoch+1}/{num_epochs}, Average Loss: {average_loss:.4f}')
+
+
+# Hyperparameters
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 4
+NUM_EPOCHS = 2
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 512
+IMAGE_WIDTH = 512
+PIN_MEMORY = True
+LOAD_MODEL = True
+TRAIN_IMG_DIR = "data/train_inputs/"
+TRAIN_GT_DIR = "data/train_gts/"
+VAL_IMG_DIR = "data/val_inputs/"
+VAL_GT_DIR = "data/val_gts/"
+MODEL_STATE_DIR = "model_state/"
+PRED_IMG_DIR = "saved_images/" 
+
+
+def train_func(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+    # model = model.float()
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets - targets.unsqueeze(1).to(device=DEVICE)
+
+        #forward
+        with torch.cuda.amp.autocast():
+            predictions = model(data.float())
+            loss = loss_fn(predictions, targets)
+
+        #backward
+        
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+    model = UNET(in_channels=1, out_channels=1).to(DEVICE)
+    loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_GT_DIR,
+        VAL_IMG_DIR,
+        VAL_GT_DIR,
+        BATCH_SIZE
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+    for epoch in range(NUM_EPOCHS):
+        train_func(train_loader, model, optimizer, loss_fn, scaler)
+
+        #save model
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict()
+        }
+        save_checkpoint(checkpoint, epoch, folder=MODEL_STATE_DIR)
+
+        #check accuracy
+        check_accuracy(val_loader, model, device=DEVICE)
+
+        save_prediction_as_imgs(
+            val_loader,model,folder=PRED_IMG_DIR,device=DEVICE
+        )
+if __name__ == "__main__":
+    main()
+
+
---
+++ neuralnet/utils.py
@@ -0,0 +1,91 @@
+import torch
+import torchvision
+from dataset import CVPDataset
+import os
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+def save_checkpoint(state, epoch, folder="model_state"):
+    filename = f"my_checkpoint_{epoch}.pth.tar"
+    filepath = os.path.join(folder,filename)
+    print("--> Saving Checkpoint to {}".format(filepath))
+    torch.save(state, filepath)
+
+def load_checkpoint(checkpoint, model):
+    print("--> Saving Checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+def get_loaders(
+        train_dir,
+        train_gt_dir,
+        val_dir,
+        val_gt_dir,
+        batch_size,
+        num_workers=4,
+        pin_memory=True
+):
+    train_ds = CVPDataset(
+        image_dir=train_dir,
+        gt_dir=train_gt_dir
+    )
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True
+    )
+
+    val_ds = CVPDataset(
+        image_dir=val_dir,
+        gt_dir=val_gt_dir
+    )
+
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True
+    )
+
+    return train_loader, val_loader
+
+def check_accuracy(loader, model, device="cuda"):
+    model.eval()
+    mse = 0
+    with torch.no_grad():
+        for x,y in loader:
+            x = x.to(device)
+            y = y.to(device)
+            preds = model(x.float())
+            mse += F.mse_loss(preds, y)
+
+    print("MSE : {}".format(mse/len(loader)))
+    model.train()
+
+def save_prediction_as_imgs(
+        loader,model, folder="saved_images",device="cuda"
+):
+    model.eval()
+    cnt = 41
+    for x,y in loader:
+        x = x.to(device)
+        with torch.no_grad():
+            preds = model(x.float())
+        for pred in preds:
+            dest_path=os.path.join(folder,f"pred_{cnt}.png")
+            torchvision.utils.save_image(
+                pred,dest_path       
+            )
+            cnt += 1
+        # torchvision.utils.save_image(
+        #     y, f"{folder}y_{idx}.png"        
+        # )
+    model.train()
+
+
+
+
+
