e47b766f5c0919d1af32210dda2cb05f1cf5268c
pillai-amal <mail.vortextv@gmail.com>
2024-02-07 16:38:07 +0200

    cleaned up a bit

--- neuralnet/train.py
+++ neuralnet/train.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-# from torch.utils.data import DataLoader
 from tqdm import tqdm
-# from model import UNetPixelwiseRegression
 from unet import UNET
 from dataset import CVPDataset
 from utils import (
@@ -14,37 +12,10 @@ from utils import (
     save_prediction_as_imgs
 )
 
-
-# def train_model(model, dataloader, criterion, optimizer, num_epochs):
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     model.to(device)
-
-#     for epoch in range(num_epochs):
-#         model.train()  # Set the model to training mode
-#         total_loss = 0.0
-
-#         with tqdm(total=len(dataloader), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
-#             for inputs, targets in dataloader:
-#                 inputs, targets = inputs.to(device), targets.to(device)
-
-#                 optimizer.zero_grad()
-#                 outputs = model(inputs)
-#                 loss = criterion(outputs, targets)
-#                 loss.backward()
-#                 optimizer.step()
-
-#                 total_loss += loss.item()
-#                 pbar.set_postfix(loss=f'{loss.item():.4f}')
-#                 pbar.update()
-
-#         average_loss = total_loss / len(dataloader)
-#         print(f'Epoch {epoch+1}/{num_epochs}, Average Loss: {average_loss:.4f}')
-
-
 # Hyperparameters
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 4
+BATCH_SIZE = 50
 NUM_EPOCHS = 2
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 512
